5c135f77baea92a53e6b9df4e0091cfb1a9a86a8
sumin0126 <ktnals2@naver.com>
2025-01-23 19:35:48 +0900

    빌드 수정

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const Document = () => {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
