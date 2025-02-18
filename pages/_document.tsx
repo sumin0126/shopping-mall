be7886d2c3b99620c40f0ff0a8d0d890437594d8
sumin0126 <ktnals2@naver.com>
2025-02-18 19:29:16 +0900

    feat: mixed content 원복

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,6 @@ const Document = () => {
     <Html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       </Head>
       <body>
         <Main />
