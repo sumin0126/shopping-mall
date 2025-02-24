68fece9d00d864ca4f5f426b7e7b40450aeea4b8
sumin0126 <ktnals2@naver.com>
2025-02-24 19:16:25 +0900

    fix: import 순서 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,5 @@
 import type { AppProps } from 'next/app';
 
-import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css'; // SCSS 충돌 방지용
-config.autoAddCss = false; // CSS 자동 추가 방지
 import { RecoilRoot } from 'recoil';
 
 import '../styles/main.scss';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css'; // SCSS 충돌 방지용
+
 import MainContainer from '@/containers/MainContainer';
 import LayoutContainer from '@/containers/layout/LayoutContainer';
+
+config.autoAddCss = false; // CSS 자동 추가 방지
 
 const index = () => {
   return (
